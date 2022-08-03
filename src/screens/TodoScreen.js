37c27d75fa87ad38ef9ee55ba68f0c2864c296b1
IlonAleks <rogilona@gmail.com>
2022-08-03 14:22:17 +0300

    убрала конфликты

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View, FlatList } from 'react-native'
 
-export default const TodoScreen = props => {
+export const TodoScreen = props => {
 	return <View>
 		<Text>Todo Screen</Text>
 
