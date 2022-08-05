1c5ff0d21a2d81bf5b59a99a77adec69ddbc0fe2
IlonAleks <rogilona@gmail.com>
2022-08-05 12:21:51 +0300

    конец 26 урока

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native'
 
-export const TodoScreen = ({ goBack }) => {
-	return <View>
-		<Text>'todo.title, который выдает ошибку('</Text>
-		<Button title='Go back' onPress={goBack} />
-	</View>
+export const TodoScreen = ({ goBack, todo }) => {
+	return (
+		<View>
+			{/* <Text>{todo.title}</Text> */}
+			<Text>'todo.title, который выдает ошибку'</Text>
+			<View style={styles.buttons}>
+				<View style={styles.button}>
+					<Button title='Go back' color='#757575' onPress={goBack} />
+				</View>
+				<View style={styles.button}>
+					<Button title='Delete' color='#e53935' onPress={console.log('To remove')} />
+				</View>
+
+			</View>
+
+		</View>
+	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	buttons: {
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	button: {
+		width: '45%'
+	}
+})
