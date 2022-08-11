b50598eb87a316779e2e490a69107a9210ba4e00
IlonAleks <rogilona@gmail.com>
2022-08-11 18:54:34 +0300

    28 урок

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native'
+import { AppCard } from '../components/ui/AppCard'
 import { THEME } from '../theme'
 
 export const TodoScreen = ({ goBack, todo }) => {
 	return (
 		<View>
-			{/* <Text>{todo.title}</Text> */}
-			<Text>'todo.title, который выдает ошибку'</Text>
+			<AppCard style={styles.card}>
+				<Text style={styles.title}>{todo.title}</Text>
+				{/* <Text style={styles.title}>'todo.title, который выдает ошибку'</Text> */}
+				<Button title='Edit' />
+			</AppCard>
+
 			<View style={styles.buttons}>
 				<View style={styles.button}>
 					<Button title='Go back' color={THEME.Danger_color} onPress={goBack} />
@@ -28,5 +33,12 @@ const styles = StyleSheet.create({
 	},
 	button: {
 		width: '45%'
+	},
+	card: {
+		padding: 15,
+		marginBottom: 20
+	},
+	title: {
+		fontSize: 20
 	}
 })
