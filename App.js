5cb372294bf61f37108b9d84d818e02f8ce223ac
IlonAleks <rogilona@gmail.com>
2022-08-17 16:59:52 +0300

    32 урок

--- App.js
+++ App.js
@@ -5,10 +5,23 @@ import { MainScreen } from './src/screens/MainScreen';
 import { TodoScreen } from './src/screens/TodoScreen';
 
 export default function App() {
-  const [todoId, setTodoId] = useState()
+  const [todoId, setTodoId] = useState(null)
   const [todos, setTodos] = useState([
-    { id: '1', title: '11111' },
-    { id: '2', title: '22222' },
+    // { id: '1', title: '11111' },
+    // { id: '2', title: '22222' },
+    // { id: '3', title: '22222' },
+    // { id: '4', title: '22222' },
+    // { id: '5', title: '22222' },
+    // { id: '6', title: '22222' },
+    // { id: '7', title: '22222' },
+    // { id: '8', title: '22222' },
+    // { id: '9', title: '22222' },
+    // { id: '10', title: '22222' },
+    // { id: '11', title: '22222' },
+    // { id: '12', title: '22222' },
+    // { id: '13', title: '22222' },
+    // { id: '14', title: '22222' },
+    // { id: '15', title: '22222' },
   ])
 
   const addTodo = (title) => {
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,18 +1,40 @@
 import React from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, View, FlatList, Text, Image } from 'react-native'
 import { AddTodo } from '../components/AddTodo'
 import { Todo } from '../components/Todo'
 
 export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
+	let content = (<FlatList //! урок 21 скрол не работает
+		keyExtractor={item => item.id.toString()}
+		data={todos}
+		renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />}
+	/>)
+
+	if (todos.length === 0) {
+		content = <View style={styles.imgWrap}>
+			<Image
+				source={require('../../assets/todo.png')}
+				style={styles.image}
+			/>
+		</View>
+	}
+
 	return <View>
 		<AddTodo onSubmit={addTodo} />
-
-		<FlatList //! урок 21 скрол не работает
-			keyExtractor={item => item.id.toString()}
-			data={todos}
-			renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />}
-		/>
+		{content}
 	</View >
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	imgWrap: {
+		// padding: 10,
+		justifyContent: 'center',
+		alignContent: 'center',
+		height: 400
+	},
+	image: {
+		height: '100%',
+		width: '100%',
+		resizeMode: 'contain'
+	}
+})
